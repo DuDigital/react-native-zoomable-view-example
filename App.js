93ef60cd3be2ae3eaa89ef34e2e614efcc68f8c4
Simon Auer <simon@du.digital>
2018-08-18 13:51:46 +0200

    improved the example

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
 import ReactNativeZoomableView from '@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView';
+
+console.disableYellowBox = true;
 
 export default class App extends React.Component {
   /**
@@ -19,28 +21,75 @@ export default class App extends React.Component {
     console.log('ZoomableEventObject: ', zoomableViewEventObject);
     console.log('');
     console.log(`Zoomed from ${zoomableViewEventObject.lastZoomLevel} to  ${zoomableViewEventObject.zoomLevel}`);
-  }
+  };
 
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <ReactNativeZoomableView
-          maxZoom={1.5}
-          minZoom={0.5}
-          zoomStep={0.5}
-          initialZoom={1}
-          bindToBorders={true}
-          onZoomAfter={this.logOutZoomState}
-          style={{
-            padding: 10,
-            backgroundColor: 'red',
-          }}
-        >
-          <Image style={{ flex: 1, width: null, height: '100%' }}
-                 source={require('./image.jpg')}
-                 resizeMode="contain" />
-        </ReactNativeZoomableView>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>React Native Zoomable View</Text>
+        </View>
+
+        <View style={styles.zoomWrapper}>
+          <ReactNativeZoomableView
+            zoomEnabled={true}
+            maxZoom={1.5}
+            minZoom={0.5}
+            zoomStep={0.25}
+            initialZoom={0.9}
+            bindToBorders={true}
+            onZoomAfter={this.logOutZoomState}
+            style={styles.zoomableView}
+          >
+            <Image
+              style={styles.image}
+              source={require('./image.jpg')}
+              resizeMode="stretch"
+            />
+            <Text style={styles.caption}>Vienna, Austria</Text>
+          </ReactNativeZoomableView>
+        </View>
       </View>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e7e7e7',
+    paddingBottom: 30,
+  },
+  header: {
+    backgroundColor: '#5569ff',
+    paddingTop: 50,
+    paddingBottom: 15,
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  zoomWrapper: {
+    flex: 1,
+    overflow: 'hidden',
+  },
+  zoomableView: {
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '80%',
+    marginBottom: 10,
+  },
+  caption: {
+    fontSize: 10,
+    color: '#444',
+    alignSelf: 'center',
+  },
+});
